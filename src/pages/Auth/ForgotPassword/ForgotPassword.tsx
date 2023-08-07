c71f4b92496ac84b6db475af6c1272d8474e55d9
Carlos marin <cmarin495@gmail.com>
2023-08-07 18:49:57 -0500

    fix forgotpass error

--- src/pages/Auth/ForgotPassword/ForgotPassword.tsx
+++ src/pages/Auth/ForgotPassword/ForgotPassword.tsx
@@ -1,15 +1,15 @@
-import Authtemplate from '../../../templates/AuthTemplate/Authtemplate';
-import { useLogin } from '../Login/hooks/useLogin';
+// import Authtemplate from '../../../templates/AuthTemplate/Authtemplate';
+// import { useLogin } from '../Login/hooks/useLogin';
 
-const ForgotPassword = () => {
-    const { handleNavigateDashboard, Email } = useLogin();
-    return (
-        <Authtemplate> 
-            FORGOT PASSWORD
-            {Email}
-            <button onClick={handleNavigateDashboard}>VOY A DASHBOARD </button>
-        </Authtemplate>
-    );
-};
+// const ForgotPassword = () => {
+//     const { handleNavigateDashboard, mail } = useLogin();
+//     return (
+//         <Authtemplate> 
+//             FORGOT PASSWORD
+//             {mail}
+//             <button onClick={handleNavigateDashboard}>VOY A DASHBOARD </button>
+//         </Authtemplate>
+//     );
+// };
 
-export default ForgotPassword;
+// export default ForgotPassword;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ForgotPassword from "~/pages/Auth/ForgotPassword/ForgotPassword";
+// import ForgotPassword from "~/pages/Auth/ForgotPassword/ForgotPassword";
 import Login from "~/pages/Auth/Login/Login";
 import Dashboard from "~/pages/Home/Dashboard/Dashboard";
 import PrivateRoute from "./PrivateRoute";
@@ -9,7 +9,7 @@ const RouterApp = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        {/* <Route path="/forgot-password" element={<ForgotPassword />} /> */}
         <Route element={<PrivateRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
         </Route>
